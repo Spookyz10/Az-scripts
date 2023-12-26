f025971419544dcd8f0fef91263f2c657e5f5371
Spookyz10 <110704273+Spookyz10@users.noreply.github.com>
2023-12-26 12:04:57 -0300

    Create Fdr Normal Specter Farm.lua

---
+++ Fdr Normal Specter Farm.lua
@@ -0,0 +1,232 @@
+repeat wait() until game:IsLoaded()
+repeat wait() until game:GetService("Players").LocalPlayer:WaitForChild('Loaded').Value == true
+
+if workspace:FindFirstChild('Falling Leaves') then
+while true do
+local args = {
+    [1] = "Raid",
+    [2] = "Frost Dragon's Rematch",
+    [3] = false
+}
+
+game:GetService("ReplicatedStorage").Events.raidEvent:FireServer(unpack(args))
+wait()
+end
+
+else do
+
+repeat wait() until
+game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(70024.8834838867188, 265.0705261230469, 28003.628173828125)
+wait(0.3)
+game.Workspace.azvaro_gamer.HumanoidRootPart.Anchored = true
+repeat wait() until
+(#game:GetService("Workspace"):WaitForChild('Mobs'):GetChildren()) > 0
+
+wait(0.5)
+game.Workspace.azvaro_gamer.HumanoidRootPart.Anchored = false
+
+local mob = game:GetService("Workspace"):WaitForChild('Mobs'):FindFirstChildWhichIsA("Model")
+
+game.Players.LocalPlayer.Character:WaitForChild('HumanoidRootPart').CFrame = mob.HumanoidRootPart.CFrame + Vector3.new(0,0,0)
+wait(0.2)
+game.Workspace.azvaro_gamer.HumanoidRootPart.Anchored = true
+wait(0.2)
+while true do
+local args = {
+    [1] = "Skill",
+    [2] = "Skill3",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+wait(0.1)
+local args = {
+    [1] = "Skill",
+    [2] = "Skill2",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "Skill",
+    [2] = "Skill2",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "Skill",
+    [2] = "Skill2",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args)) 
+wait(0.3)
+
+local args = {
+    [1] = "Skill",
+    [2] = "Skill2",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+wait(2.5)
+local args = {
+    [1] = "Skill",
+    [2] = "Skill3",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+
+local args = {
+    [1] = "Skill",
+    [2] = "Skill2",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "Skill",
+    [2] = "Skill3",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+
+local args = {
+    [1] = "Skill",
+    [2] = "Skill2",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "Skill",
+    [2] = "Skill3",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+
+local args = {
+    [1] = "Skill",
+    [2] = "Skill2",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "Skill",
+    [2] = "Skill3",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+
+local args = {
+    [1] = "Skill",
+    [2] = "Skill2",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+wait(2.5)
+local args = {
+    [1] = "Skill",
+    [2] = "Skill3",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "Skill",
+    [2] = "Skill2",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "Skill",
+    [2] = "Skill2",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "Skill",
+    [2] = "Skill2",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "Skill",
+    [2] = "Skill2",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+wait(2.5)
+local args = {
+    [1] = "Skill",
+    [2] = "Skill3",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "Skill",
+    [2] = "Skill2",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+wait(0.3)
+
+local args = {
+    [1] = "Skill",
+    [2] = "Skill2",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "Skill",
+    [2] = "Skill2",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "Skill",
+    [2] = "Skill2",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "Skill",
+    [2] = "Skill1",
+    [3] = "Attack"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("attack"):FireServer(unpack(args))
+wait(0.5)
+  end
+  end
+  end
